62ae3fcf10100f4d83f7b3db72794eb579a51a0f
Rafael Batista <rafaelb24@yahoo.com>
2018-12-01 00:02:53 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name)
+VALUES ("Mr. Chocolate");
+
+INSERT INTO bears (name)
+VALUES ("Rowdy");
+
+INSERT INTO bears (name)
+VALUES ("Tabitha");
+
+INSERT INTO bears (name)
+VALUES ("Segeant Brown");
+
+INSERT INTO bears (name)
+VALUES ("Melissa");
+
+INSERT INTO bears (name)
+VALUES ("Grinch");
+
+INSERT INTO bears (name)
+VALUES ("Wendy");
+
+INSERT INTO bears (name)
+VALUES (NULL);
+
