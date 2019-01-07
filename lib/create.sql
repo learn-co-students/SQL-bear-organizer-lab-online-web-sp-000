5a702bac2bc5a0c33a32c862f1902dce27cbacc1
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-07 18:39:04 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE bears
+  (id INTEGER primary key,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive INTEGER);
---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+insert into bears (name, age, gender, color, temperament, alive)
+  values ('Mr. Chocolate', 22, 'female', 'brown', 'shy', 1);
+insert into bears (name, age, gender, color, temperament, alive)
+  values ('Rowdy', 14, 'female', 'ginger', 'shy', 1);
+insert into bears (name, age, gender, color, temperament, alive)
+  values ('Tabitha', 3, 'male', 'ginger', 'curious', 1);
+insert into bears (name, age, gender, color, temperament, alive)
+  values ('Sergeant Brown', 6, 'male', 'honey', 'curious', 0);
+insert into bears (name, age, gender, color, temperament, alive)
+  values ('Melissa', 26, 'female', 'chestnut', 'playful', 0);
+insert into bears (name, age, gender, color, temperament, alive)
+  values ('Grinch', 2, 'male', 'honey', 'shy', 1);
+insert into bears (name, age, gender, color, temperament, alive)
+  values ('Wendy', 13, 'female', 'chestnut', 'playful', 1);
+insert into bears (name, age, gender, color, temperament, alive)
+  values (null, 18, 'male', 'brown', 'angry', 1);
