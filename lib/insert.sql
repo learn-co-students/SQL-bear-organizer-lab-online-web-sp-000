8aefecf77a1c49768a5bf717aa7f61f1ca1812dd
Jessica Gladstone <jcg321@comcast.net>
2019-01-20 23:02:57 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears 
+ VALUES (1,"Mr. Chocolate", 12, "male", "brown", "mellow", 0);
+
+INSERT INTO bears VALUES (2,"Rowdy", 20, "male", "black", "aggressive", 0);
+INSERT INTO bears VALUES (3,"Tabitha", 5, "female", "brown", "friendly", 0);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 35, "male", "grey", "aggressive", 1);
+INSERT INTO bears VALUES (5,"Melissa", 22, "female", "black", "mellow", 0);
+INSERT INTO bears VALUES (6,"Grinch", 9, "male", "grey", "independent", 0);
+INSERT INTO bears VALUES (7,"Wendy", 14, "female", "white", "friendly", 0);
+INSERT INTO bears VALUES (8,NULL, 5, "male", "black", "aggressive", 1);
