bf7fc9662ad3f32599458e1a307bcff21f7b394c
davisjustinw <davisjustinw@gmail.com>
2019-03-09 12:01:44 -0800

    all tests pass

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 20, "m", "brown", "aggressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Rowdy", 2, "m", "red", "playful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Tabitha", 10, "f", "yellow", "unknown", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Sergeant Brown", 22, "m", "brown", "strict", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Melissa", 8, "f", "pink", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Grinch", 4, "m", "green", "grumpy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Wendy", 1, "f", "blue", "friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES (NULL, 6, "m", "brown", "aggressive", 0);
