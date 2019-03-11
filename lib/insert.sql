7d7108575b77f1260e90b85ae3ca3cfc6216b6d1
Tommy Curley <thomas.curley93@gmail.com>
2019-03-11 14:41:46 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("bear1", 1, "male", "brown", "aggressive", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("bear2", 1, "male", "grizzly", "friendly", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("bear3", 1, "male", "white", "calm", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("bear4", 1, "male", "black", "aggressive", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("bear5", 1, "male", "brown", "aggressive", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("bear6", 3, "male", "brown", "aggressive", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("bear7", 13, "male", "black", "calm", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES (NULL, 9, "female", "grizzly", "friendly", true);
