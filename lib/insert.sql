f402de5d6875408a6c4f7559f66d813430b3ff54
Frederic Silber <freddy.silber@cloudcoach.com>
2019-07-20 01:44:32 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+-- INSERT INTO cats (name, age, breed, net_worth) VALUES ("Maru", 3, "Scottish Fold", 1000000);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Andy", 50, "M", "brown", "chill", 1),
+                                                                        ("Bri", 12, "F", "black", "kind", 1),
+                                                                        ("CarlosJose", 78, "M", "brown", "chill", 1),
+                                                                        ("Tooga", 26, "M", "brown", "chill", 0),
+                                                                        ("Quincy", 34, "M", "brown", "chill", 0),
+                                                                        (NULL, 18, "female", "brown", "goofy", 1),
+                                                                        ("Cody", 39, "female", "grizzly", "homie", 0),
+                                                                        ("Harry", 19, "male", "black", "aggressive", 1);
