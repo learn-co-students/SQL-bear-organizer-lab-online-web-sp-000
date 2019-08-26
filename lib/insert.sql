def29f6a3668224a3a3d875d29c485671d596ae8
Lindsey Wells <lindsey.wells84@gmail.com>
2019-08-26 13:12:59 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,23 +1,23 @@
 INSERT INTO bears(name, age, gender, color, temperament, alive)
-VALUES ("Mr Chocolate", 11, "M", "Brown", "Calm", 1)
+VALUES ("Mr Chocolate", 11, "M", "Brown", "Calm", 1);
 
 INSERT INTO bears(name, age, gender, color, temperament, alive)
-VALUES ("Rowdy", 4, "M", "Brown", "Aggressive", 0)
+VALUES ("Rowdy", 4, "M", "Brown", "Aggressive", 0);
 
 INSERT INTO bears(name, age, gender, color, temperament, alive)
-VALUES ("Tabitha", 9, "F", "Blue", "Chilled", 1)
+VALUES ("Tabitha", 9, "F", "Blue", "Chilled", 1);
 
 INSERT INTO bears(name, age, gender, color, temperament, alive)
-VALUES ("Sergeant Brown", 8, "M", "Yellow", "Calm", 0)
+VALUES ("Sergeant Brown", 8, "M", "Yellow", "Calm", 0);
 
 INSERT INTO bears(name, age, gender, color, temperament, alive)
-VALUES ("Melissa", 23, "F", "Black", "Aggressive", 1)
+VALUES ("Melissa", 23, "F", "Black", "Aggressive", 1);
 
 INSERT INTO bears(name, age, gender, color, temperament, alive)
-VALUES ("Grinch", 15, "M", "Brown", "Sleepy", 1)
+VALUES ("Grinch", 15, "M", "Brown", "Sleepy", 1);
 
 INSERT INTO bears(name, age, gender, color, temperament, alive)
-VALUES ("Wendy", 6, "F", "White", "Calm", 1)
+VALUES ("Wendy", 6, "F", "White", "Calm", 1);
 
 INSERT INTO bears(name, age, gender, color, temperament, alive)
-VALUES (NULL, 6, "F", "White", "Murderous", 0)
+VALUES (NULL, 6, "F", "White", "Murderous", 0);
