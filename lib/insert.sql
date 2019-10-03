0bc0fc40b1d78e56516cc512ee8ae8c119aeb940
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-10-03 01:40:20 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+("Mr. Chocolate", 17, "male", "brown", "laid back", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+("Rowdy", 10, "male", "brown", "aggresive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+("Tabitha", 12, "female", "brown", "cautious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+("Sergeant Brown", 17, "male", "brown", "aggresive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+("Melissa", 20, "female", "brown", "lazy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+("Grinch", 2, "male", "black", "playful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+("Wendy", 9, "female", "brown", "playful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+(NULL, 17, "male", "brown", "dead", 0);
