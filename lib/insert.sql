c36e634e88e6cc84ebe449dc45c7707d381c2b41
Richard Lee <richlee81192@gmail.com>
2019-10-10 21:59:36 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,22 @@
+-- "INSERT INTO bears(*) VALUES ('Apple', 10, 'male', 'black', 'calm', 'true');"
+
+-- "INSERT INTO bears(*) VALUES ('Banana', 10, 'male', 'black', 'calm', 'true');"
+
+-- "INSERT INTO bears(*) VALUES ('Crisp', 10, 'male', 'black', 'calm', 'true');"
+
+-- "INSERT INTO bears(*) VALUES ('Diablo', 10, 'male', 'black', 'calm', 'true');"
+
+-- "INSERT INTO bears(*) VALUES ('Echo', 10, 'male', 'black', 'calm', 'true');"
+-- "INSERT INTO bears(*) VALUES ('Frank', 10, 'male', 'black', 'calm', 'true');"
+-- "INSERT INTO bears(*) VALUES ('Gaul', 10, 'male', 'black', 'calm', 'true');"
+-- "INSERT INTO bears(*) VALUES (NULL, 10, 'male', 'black', 'calm', 'true');"
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
