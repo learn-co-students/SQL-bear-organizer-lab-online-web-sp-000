89ef7452494711539214e8099f0d3789c63f1028
Panela Leung <leungpanela@gmail.com>
2019-12-31 07:03:26 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES("Mr.Chocolate", 1, "F", "brown", "calm", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES("Rowdy", 2, "F", "brown", "calm", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES("Tabitha", 1, "M", "black", "calm", 0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES("Sergeant Brown", 3, "F", "brown", "calm", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES("Melissa", 4, "M", "black", "calm", 0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES("Grinch", 1, "F", "brown", "calm", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES("Wendy", 2, "M", "black", "calm", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES( NULL, 3, "F", "brown", "calm", 0);
