8a3d59e0600b7b19c7b67fcebeec459a0e428c65
Ronda D Cowen <cowenhome5@gmail.com>
2020-01-27 23:21:03 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,11 +19,13 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT MAX color COUNT (*) FROM bears;"
+  # "SELECT MAX color FROM bears;"
+  "SELECT COUNT (*) MAX color FROM bears;"
+
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT "
 end
 
 def selects_bear_that_killed_Tim
