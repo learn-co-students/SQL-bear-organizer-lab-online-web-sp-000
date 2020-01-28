9a5bb1f192f63c3ebedeb51b74289fc01b544b78
Ronda D Cowen <cowenhome5@gmail.com>
2020-01-28 01:14:10 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,13 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  # "SELECT MAX, COUNT (color) FROM bears;"
-  "SELECT COUNT (color) FROM bears ORDER BY COUNT;"
-#   SELECT MAX (color) 
-# FROM (SELECT AVG,COUNT(agent_code) mycount
-# FROM orders
-# GROUP BY agent_code);
-
+  "SELECT COUNT (color) FROM bears, ORDER BY count, DESC LIMIT 1;" 
 end
 
 def counts_number_of_bears_with_goofy_temperaments
