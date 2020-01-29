dc325bac3c70f3cbed590f41782f78676543f63a
Sergio Marelli <sergio.mrll@gmail.com>
2020-01-29 21:41:15 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,128 @@
+INSERT INTO bears (
+	name,
+	age,
+	gender,
+	color,
+	temperament,
+	alive
+) VALUES (
+	'Mr. Chocolate',
+	8,
+	'male',
+	'red',
+	'docile',
+	1
+);
+
+INSERT INTO bears (
+	name,
+	age,
+	gender,
+	color,
+	temperament,
+	alive
+) VALUES (
+	'Rowdy',
+	8,
+	'male',
+	'red',
+	'docile',
+	1
+);
+
+INSERT INTO bears (
+	name,
+	age,
+	gender,
+	color,
+	temperament,
+	alive
+) VALUES (
+	'Tabitha',
+	6,
+	'female',
+	'red',
+	'docile',
+	1
+);
+
+INSERT INTO bears (
+	name,
+	age,
+	gender,
+	color,
+	temperament,
+	alive
+) VALUES (
+	'Sergeant Brown',
+	8,
+	'male',
+	'red',
+	'docile',
+	1
+);
+
+INSERT INTO bears (
+	name,
+	age,
+	gender,
+	color,
+	temperament,
+	alive
+) VALUES (
+	'Melissa',
+	13,
+	'female',
+	'red',
+	'docile',
+	1
+);
+
+INSERT INTO bears (
+	name,
+	age,
+	gender,
+	color,
+	temperament,
+	alive
+) VALUES (
+	'Wendy',
+	6,
+	'female',
+	'red',
+	'docile',
+	1
+);
+
+INSERT INTO bears (
+	name,
+	age,
+	gender,
+	color,
+	temperament,
+	alive
+) VALUES (
+	'Grinch',
+	8,
+	'male',
+	'red',
+	'docile',
+	1
+);
+
+INSERT INTO bears (
+	name,
+	age,
+	gender,
+	color,
+	temperament,
+	alive
+) VALUES (
+	NULL,
+	8,
+	'male',
+	'red',
+	'docile',
+	1
+);
+
