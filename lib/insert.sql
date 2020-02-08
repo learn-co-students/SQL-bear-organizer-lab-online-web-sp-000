ab3f5c10e09f5918d0f6f8b9f261a35e4c1b7e2d
Jason J Cruz <jaycru06@yahoo.com>
2020-02-08 03:41:54 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -12,6 +12,9 @@ INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Mr. Choc
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Rowdy", 44, "Female", "Dark Red", "Wild", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Tabitha", 33, "Female", "Light Red", "Sweet", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Sergeant Brown", 54, "Male", "Brown", "Stern", 0);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Melissa", 24, "Female", "Pink", "Calm", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Grinch", 44, "Male", "Dark Green", "Angry", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Wendy", 29, "Female", "Light Brown", "Nice", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES(NULL, NULL, NULL, NULL, NULL, NULL);
 
 
