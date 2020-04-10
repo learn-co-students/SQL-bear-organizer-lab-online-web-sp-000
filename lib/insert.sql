445e3793503086309215927bd9098519e6ef196a
doakcovington <doakcovington@protonmail.com>
2020-04-09 18:54:44 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("MR. Chocolate", 15, "Male", "Brown", "Sweet", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ('Rowdy', 5, 'Male', 'Brown', 'Calm', True);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ('Tabitha', 10, 'Female', 'Brown', 'Protective', True);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ('Sergeant Brown', 20, 'Male', 'Brown', 'Grizzled', True);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ('Melissa', 2, 'Female', 'Brown', 'Inquisitive', True);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ('Grinch', 25, 'Male', 'Black', 'Joyful', False);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ('Wendy', 10, 'Female', 'Black', 'Playful', True);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES (Null, 25, 'Male', 'Brown', 'Hungry', False);
