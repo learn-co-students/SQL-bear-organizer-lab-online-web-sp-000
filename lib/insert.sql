1d57dbea1e5a9afa8c0b3115735da1b419f33e90
bibraj96 <blendi.ibraj96@gmail.com>
2020-05-01 13:12:47 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Bowie", 18, "M", "Light Brown", "Calm", 0);
+INSERT INTO bears VALUES (2, "Bjork", 14, "F", "Black", "Friendly", 1);
+INSERT INTO bears VALUES (3, "William", 10, "M", "Dark Gray", "Quiet", 1);
+INSERT INTO bears VALUES (4, "Sade", 13, "F", "Brown", "Playful", 1);
+INSERT INTO bears VALUES (5, "Frank", 18, "M", "Black", "Joyful", 1);
+INSERT INTO bears VALUES (6, null, 15, "F", "Blue", "Instense", 1);
+INSERT INTO bears VALUES (7, "Elliott", 13, "M", "Green", "Calm", 0);
+INSERT INTO bears VALUES (8, "Julia", 17, "F", "Dark Blue", "Affectionate", 1);
