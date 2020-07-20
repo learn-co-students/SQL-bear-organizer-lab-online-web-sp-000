ad229e40edcf01922c76967497860c0b5f40bb73
ems_codes <emilyxi@gmail.com>
2020-07-20 17:54:30 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('1','Mr. Chocolate','4','male','brown',
+'happy','1');
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('2','Rowdy','2','male','brown',
+'sad','1');
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('3','Tabitha','6','female','brown',
+'dead','0');
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('4','Sergeant Brown','male','1','brown',
+'happy','1');
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('5','Melissa','13','female','black',
+'dead','0');
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('6','Grinch','9','male','black',
+'sad','1');
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('7','Wendy','6','female','black',
+'dead','0');
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ('8', NULL,NULL,NULL,
+NULL,NULL,NULL);
