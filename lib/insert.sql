e514ec31156352428325055c5b394effebb6fcb2
Olivia Cubela <oliviacubela@gmail.com>
2020-08-25 17:56:30 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, "Mr. Chocolate", 3, "male", "brown", "mild-mannered", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Rowdy", 10, "female", "black", "shy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Tabitha", 30, "female", "brown", "extroverted", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Sergeant Brown", 53, "female", "white", "cranky", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Melissa", 4, "female", "white", "calm", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Grinch", 16, "male", "green", "crazy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Wendy", 7, "female", "brown", "chill", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, NULL, 3, "male", "brown", "violent", 0);
+
+
+
+
