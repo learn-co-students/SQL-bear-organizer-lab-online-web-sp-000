9569bb697d500aec3c6ba5c89d7d92e76a510174
jrog20 <jaimerogstad@gmail.com>
2020-09-02 09:19:22 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+-- Boolean = stored as integers 0 (false) and 1 (true).
+
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    gender TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive INTEGER,
+);
---
+++ lib/insert.sql
@@ -0,0 +1,1 @@
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "brown", "Gentleman", 1);
