209e9524c0b6de09d1e6904307d858a558556fed
DabooUA <sbardysh@tutanota.com>
2020-09-16 22:00:13 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 10, "male", "Brown", "Non Agrassive", 1);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 9, "male", "Brown", "Goofy", 1);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 12, "female", "Brown", "Protective", 1);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 15, "male", "Brown", "Da Boss", 1);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 10, "female", "Black", "Goofy", 1);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 6, "male", "Brown", "Goofy", 1);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 11, "female", "Black", "Laidback", 1);
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 18, "male", "Brown", "Unkown", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
+
+
+
+
+
+
+
