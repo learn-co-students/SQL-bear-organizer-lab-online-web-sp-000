5e9906dce47729a84ea1730c3cc13ed813c98384
Alla Kuzminova <alchik@gmail.com>
2021-01-20 20:44:08 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES(1, "Mr. Chocolate", 13, "Male", "dark brown", "happy", 1);
+INSERT INTO bears VALUES(2, "Rowdy", 5, "Male", "black", "rowdy", 1);
+INSERT INTO bears VALUES(3, "Tabitha", 3, "Femaile", "brown", "gentle", 1);
+INSERT INTO bears VALUES(4, "Sergent Brown", 6, "Male", "white", "calm", 1);
+INSERT INTO bears VALUES(5, "Melissa", 11, "Female", "white and black", "calm", 1);
+INSERT INTO bears VALUES(6, "Grinch", 12, "Male", "black", "angry", 1);
+INSERT INTO bears VALUES(7, "Wendy", 6, "Female", "white", "kind", 1);
+INSERT INTO bears (id, age, gender, color, temperament, alive) VALUES(8, 6, "male", "dark brown", "aggressive", 1);
