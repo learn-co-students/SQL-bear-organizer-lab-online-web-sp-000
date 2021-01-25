8b46c1e5f2f9b2f7ed066dae1e2408b99dc376a4
carolyn-manning <carolyn.manning3@gmail.com>
2021-01-24 19:35:26 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+    ('Mr. Chocolate', 1, 'M', 'brown', 'mild', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+    ('Rowdy', 2, 'M', 'brown', 'mild', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+    ('Tabitha', 2, 'F', 'black', 'goofy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+    ('Sergeant Brown', 4, 'M', 'black', 'agressive', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+    ('Melissa', 1, 'F', 'brown', 'mild', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+    ('Grinch', 5, 'M', 'brown', 'goofy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+    ("Grinch", 2, 'F', 'black', 'aggresive', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+    (NULL, 6, 'F', 'black', 'aggressive', 0);
