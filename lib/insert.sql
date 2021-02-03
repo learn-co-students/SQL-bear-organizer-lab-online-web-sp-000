38d1803db4c750c29858f5b66c4fdcd5a4691d58
dbrandt1990 <dvbrandt90@gmail.com>
2021-02-02 22:19:00 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears
+VALUES(0, "Mr.Chocolate", 12, "M", "brown", "happy", TRUE),
+(1, "Rowdy", 12, "M", "brown", "happy", TRUE),
+(2, "Tabitha", 12, "F", "brown", "happy", TRUE),
+(3, "Sargent Brown", 12, "M", "brown", "happy", TRUE),
+(4, "Melissa", 12, "F", "brown", "happy", TRUE),
+(5, "Grinch", 12, "M", "brown", "happy", TRUE),
+(6, "Wendy", 12, "F", "brown", "happy", TRUE),
+(7, NULL, 12, "M", "brown", "happy", TRUE);
