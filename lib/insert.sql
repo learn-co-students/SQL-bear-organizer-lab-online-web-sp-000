d1ea03350e508d18df1229fd791955290d8cecdb
GoldenPavilion <rcorystine@gmail.com>
2021-02-15 21:31:40 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 17, 'male', 'brown', 'gregarious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'male', 'black', 'rowdy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 23, 'female', 'brown', 'lazy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 28, 'male', 'black', 'standoffish', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 14, 'female', 'black', 'curious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 32, 'male', 'brown', 'grumpy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 18, 'female', 'black', 'curious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 28, 'male', 'white', 'sick', 0);
