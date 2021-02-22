b49f80f55c4cc89ca864a4e043312e2daba38cc2
danny galimi <dannygalimi13@aim.com>
2021-02-22 04:10:56 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Smokey', 3, 'male', 'black', 'playful', 'true' );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Stinky', 5, 'male', 'blue', 'angry', 'true' );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Snarky', 6, 'female', 'black', 'sassy', 'true' );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Savage', 4, 'male', 'brown', 'viscious', 'true' );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sweetheart', 3, 'female', 'green', 'warm', 'true' );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sucker', 1, 'male', 'red', 'naive', 'true' );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Swindler', 6, 'female', 'black', 'suspicious', 'true' );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 7, 'male', 'yellow', 'ditzy', 'true' );
