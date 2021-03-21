ae82057d0ee6c91e9bc05f50d028c49132b39b24
Shami Tomita <shamitomita@gmail.com>
2021-03-21 21:54:41 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 20, "male", "chocolate", "gentle", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Rowdy", 10, "trans icon", "light brown", "cute", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Tabitha", 2, "female", "rainbow", "sweet", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Sergeant Brown", 60, "male", "tan", "actually rowdy", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Melissa", 20, "female", "black", "agressive", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Grinch", 90, "nb", "army green", "binch", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+("Weny", 1, "baby", "translucent", "unknown", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
+(NULL, NULL, NULL, "black", "blood thristy", 1);
