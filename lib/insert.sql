75fd6e72e2e0136546f76e813cc3bbb3b23f8dc2
Jouse then <jthen93@gmail.com>
2021-03-25 03:56:21 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,33 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 12, 'Male', "Pink", "Pissy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Rowdy', 1, 'Male', "Oranange", "Chill", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Tabitha', 4, 'Undefined', "Brown", "Lovely", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Sergeant Brown', 12, 'Female', "White", "Annoying", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Melissa', 28, 'Female', "Brown", "Nice Kinda", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Grinch', 120, 'Male', "Green", "Mean", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ('Wendy', 2, 'Female', "Red", "Tasty", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES (null, 12, 'Male', "Brown", "Chill AF", 1);
+
+
+
+
+
+
+
+
+
+
