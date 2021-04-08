a621c699c13cfd73abd09d61c089d610ecc17697
Dobakito <77289937+Dobakito@users.noreply.github.com>
2021-04-08 18:54:58 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (1, 'Mr. Chocolate', 12, 'M', 'brown', 'docile', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (2, 'Rowdy', 8, 'M', 'grey', 'aggressive', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (3, 'Tabitha', 14, 'F', 'black', 'goofy', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (4, 'Sargeant Brown', 9, 'M', 'brown', 'authoritarian', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (5, 'Melissa', 11, 'F', 'green', 'chill', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (6, 'Grinch', 5, 'F', 'green', 'mean', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (7, 'Wendy', 14, 'F', 'red', 'loving', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+  VALUES (8, null, 10, 'M', 'black', 'aggressive', 0);
