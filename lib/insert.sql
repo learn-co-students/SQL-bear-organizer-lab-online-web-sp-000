dae4cd9049046066cbb403d02e3e0b0e3cdb4bbf
Daysha Jones <daysha.vu@icloud.com>
2021-05-10 22:50:33 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1, "Mr.Chocolate", 5, "M", "White", "Happy", "true"),
+(2, "Rowdy", 4, "F", "Black", "Silly", "true"),
+(3, "Tabitha", 3, "F", "Brown", "Warm", "true"),
+(4, "Sergeant Brown", 6, "M", "White", "Strict", "true"),
+(5, "Melissa", 5, "F", "Black", "Good", "true"),
+(6, "Grinch", 3, "M", "Green", "Mean", "true"),
+(7, "Wendy", 5, "F", "White", "Happy", "true"),
+(8, null, 5, "M", "White", "Agressive", "false");
