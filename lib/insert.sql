3cf1df92b700f5e66ab2b8f04723d5bc387776f2
Drew Keat <thedrewkeat@gmail.com>
2021-05-22 22:27:27 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocoloate", 10, "Male", "Brown", "Agreeable", 1);
+INSERT INTO bears (name, age, color, temperament, alive) VALUES ("Rowdy", 9, "Black", "Agreeable", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 8, "Male", "Brown", "Agreeable", 0);
+INSERT INTO bears (name, age, color, temperament, alive) VALUES ("Sergeant Brown", 7, "Brown", "Agreeable", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 6, "Male", "Black", "Agreeable", 1);
+INSERT INTO bears (name, gender, color, temperament, alive) VALUES ("Grinch", "Female", "Brown", "Agreeable", 0);
+INSERT INTO bears (name, gender, color, temperament, alive) VALUES ("Wendy", "Female", "Black", "Agreeable", 1);
+INSERT INTO bears (name, gender, color, temperament, alive) VALUES (NULL, "Female", "Brown", "Agreeable", 1);
